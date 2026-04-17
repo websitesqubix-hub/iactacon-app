3b6bf47882b2a16cadddc1fe3af393845f3e7587
Akash <websites.qubix@gmail.com>
2026-04-17 13:05:13 +0530

    fix manifest type

---
+++ app/manifest.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "IACTACON 2027",
+    short_name: "IACTACON",
+    description: "Official Conference App",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#0a2a6e",
+    theme_color: "#0a2a6e",
+    orientation: "portrait",
+    icons: [
+      {
+        src: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
